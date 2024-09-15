01a1233f5c7652270242c5d5129d21b18d4b650c
maya-nachiappan <maya.n102304@gmail.com>
2024-09-15 10:50:00 -0700

    printing values

--- CIS25 - assignment 5/main.cpp
+++ CIS25 - assignment 5/main.cpp
@@ -14,13 +14,29 @@ int main ()
     short sMin;
     short sMax;
     
+    // for loop to get min value of short using overloading
     for (short i=-1; i<0; i--)
     {
         sMin=i;
     }
+    // loop to get max value of short
     for (short i=1; i>0; i++)
     {
         sMax=i;
     }
+    cout << "The minimum value of a short data type is: " <<sMin << " and the maximum is: " << sMax << endl;
+    
+    // loop to get min value of double
+    for (double i=-1; i<0; i--)
+    {
+        dMin=i;
+    }
+    
+    // loop to get max value of double
+    for (double i=1; i>0; i++)
+    {
+        dMax=i;
+    }
+    cout<<  "The minimum value of a double data type is: " <<dMin << " and the maximum value is: " << dMax << endl;
 }
 
